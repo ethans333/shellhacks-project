06f0383d94dbe3c9bcac14f0e5f4fd562ce3494e
ethans333 <gamesniper321@gmail.com>
2023-09-17 02:14:22 -0400

    jskdjfkdso

---
+++ src/Components/QuizQuestion.jsx
@@ -0,0 +1,14 @@
+export default function QuizQuestion() {
+  return (
+    <div>
+      <div className="mb-2">
+        Ipsum officia amet duis eu proident Lorem et dolor?
+      </div>
+      <div className="mb-10 pl-1">
+        <div className="quiz-question">Ipsum officia amet duis eu proident</div>
+        <div className="quiz-question">Ipsum officia amet duis eu proident</div>
+        <div className="quiz-question">Ipsum officia amet duis eu proident</div>
+      </div>
+    </div>
+  );
+}
--- src/Learn.jsx
+++ src/Learn.jsx
@@ -1,30 +1,49 @@
 import NavBar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import QuizPreview from "./Components/QuizPreview";
+import QuizQuestion from "./Components/QuizQuestion";
+
+import { useState } from "react";
 
 export default function Learn() {
+  const [selectedQuiz, setSelectedQuiz] = useState(-1);
+
   return (
     <div>
       <NavBar />
       <div className="center-page">
-        <div className="lg:w-[45vw] mt-12">
-          <div className="text-2xl font-bold mb-8">Find a Quiz</div>
-          <QuizPreview
-            title="Aliquip ut irure aute."
-            desc="Aliquip ut irure aute consequat duis duis aliquip ex ut duis duis aliquip ex ut."
-            category="Losfge"
-          />
-          <QuizPreview
-            title="Aliquip ut irure aute."
-            desc="Aliquip ut irure aute consequat duis duis aliquip ex ut duis duis aliquip ex ut."
-            category="Losfge"
-          />
-          <QuizPreview
-            title="Aliquip ut irure aute."
-            desc="Aliquip ut irure aute consequat duis duis aliquip ex ut duis duis aliquip ex ut."
-            category="Losfge"
-          />
-        </div>
+        {/* Quiz Selection */}
+        {selectedQuiz == -1 ? (
+          <div className="lg:w-[45vw] mt-12">
+            <div className="text-2xl font-bold mb-8">Find a Quiz</div>
+            <div onClick={() => setSelectedQuiz(0)}>
+              <QuizPreview
+                title="Aliquip ut irure aute."
+                desc="Aliquip ut irure aute consequat duis duis aliquip ex ut duis duis aliquip ex ut."
+                category="Losfge"
+              />
+            </div>
+          </div>
+        ) : (
+          // Article
+          <div className="lg:w-[66vw] mt-16 grid grid-cols-1 lg:grid-cols-5 lg:gap-x-10">
+            <div className="col-span-3 lg:border-r pr-10 ml-5 lg:ml-0">
+              <h2>Article Title</h2>
+              <div className="mt-3">
+                Ut laboris adipisicing tempor fugiat occaecat ea. Nostrud quis
+                ullamco dolore eiusmod occaecat dolor. Ipsum culpa magna
+                consectetur sit sint reprehenderit occaecat do aliqua tempor
+                nisi qui ullamco pariatur...
+              </div>
+            </div>
+            <div className="col-span-2 mt-32 lg:mt-0 mx-5 lg:mx-0">
+              <h1 className="mb-10">Done reading? Quiz Yourself!</h1>
+              <QuizQuestion />
+              <QuizQuestion />
+              <QuizQuestion />
+            </div>
+          </div>
+        )}
         <Footer />
       </div>
     </div>
